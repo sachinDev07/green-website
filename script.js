8fe8d83a73b3ddd80802822541894c7cc38ccdf5
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2022-10-20 21:13:48 +0500

    - added toggle code -

---
+++ script.js
@@ -0,0 +1,26 @@
+const inputEl = document.getElementById('toggle__btn');
+const month = document.querySelector('.mon');
+const year = document.querySelector('.year');
+
+inputEl.checked = JSON.parse(localStorage.getItem("mode"));
+updateColor();
+
+function updateColor() {
+    if(inputEl.checked) {
+        year.style.color = "black";
+        month.style.color = "";
+    }
+    else {
+        month.style.color = "black";
+        year.style.color = "";
+    }
+}
+
+inputEl.addEventListener("input", ()=> {
+    updateColor();
+    updateLocalStorage();
+})
+
+function updateLocalStorage() {
+    localStorage.setItem("mode", JSON.stringify(inputEl.checked));
+}
