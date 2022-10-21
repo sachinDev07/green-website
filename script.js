34ec3710e7523d6f0fa00c97bc177f6effcf081c
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2022-10-21 13:47:55 +0500

    - added price change functionality -

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 const inputEl = document.getElementById('toggle__btn');
 const month = document.querySelector('.mon');
 const year = document.querySelector('.year');
+const yearly = document.querySelectorAll('.yearly');
+const monthly = document.querySelectorAll('.monthly');
 
-// inputEl.checked = JSON.parse(localStorage.getItem("mode"));
 updateColor();
 
 function updateColor() {
     if(inputEl.checked) {
+        monthly.forEach(mo => {mo.style.display = 'none'});
+        yearly.forEach(mo => {mo.style.display = 'block'});
+
         year.style.color = "black";
         month.style.color = "";
+        
     }
     else {
+        monthly.forEach(mo => {mo.style.display = 'block'});
+        yearly.forEach(mo => {mo.style.display = 'none'});
+
         month.style.color = "black";
         year.style.color = "";
     }
@@ -19,7 +27,3 @@ function updateColor() {
 inputEl.addEventListener("input", ()=> {
     updateColor();
 })
-
-// function updateLocalStorage() {
-//     localStorage.setItem("mode", JSON.stringify(inputEl.checked));
-// }
